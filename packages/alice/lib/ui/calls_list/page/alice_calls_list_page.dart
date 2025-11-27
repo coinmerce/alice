8bc06c003b16d74186b6c10649b1c746f740552f
Miroslav Skoda <miroslav.skoda@coinmerce.io>
2025-11-27 10:14:11 +0100

    open_filex removal

--- packages/alice/lib/ui/calls_list/page/alice_calls_list_page.dart
+++ packages/alice/lib/ui/calls_list/page/alice_calls_list_page.dart
@@ -17,7 +17,6 @@ import 'package:alice/ui/common/alice_page.dart';
 import 'package:alice/ui/calls_list/widget/alice_logs_screen.dart';
 import 'package:alice/ui/common/alice_theme.dart';
 import 'package:flutter/material.dart';
-import 'package:open_filex/open_filex.dart';
 
 /// Page which displays list of calls caught by Alice. It displays tab view
 /// where calls and logs can be inspected. It allows to sort calls, delete calls
@@ -261,9 +260,7 @@ class _AliceCallsListPageState extends State<AliceCallsListPage>
             OperatingSystem.isAndroid
                 ? context.i18n(AliceTranslationKey.saveSuccessView)
                 : null,
-        secondButtonAction:
-            () =>
-                OperatingSystem.isAndroid ? OpenFilex.open(result.path!) : null,
+        secondButtonAction: () => null,
       );
     } else {
       final [String title, String description] = switch (result.error) {
